dab7f961cbca688c1c55dadb10931a857ba05151
Espen Meidell <espen.meidell@gmail.com>
2016-03-13 11:58:00 +0100

    Chart data generated

--- app.js
+++ app.js
@@ -42,25 +42,55 @@ app.get("/", client.secured, function (req, res, next) {
         });
     console.log("Prøver å hente noe shit:")
 
-    request.get('https://api.sandbox.sparebank1.no/api/v1/transactions/').
+    request.get('https://api.sandbox.sparebank1.no/api/v1/transactions/all').
       set('Authorization', 'Bearer ' + accessToken)
       .end(function (err, result, body) {
           if (err) {
               return next(err);
           }
           log = JSON.parse(result.text)
-          var stats = {}
-          for (var i = 0; i < log.transactions.length; i++) {
-            log.transactions[i].description = log.transactions[i].description.toLowerCase()
-            if (log.transactions[i].description in stats){
-              stats[log.transactions[i].description] += 1
-            } else {
-              stats[log.transactions[i].description] = 1
-            }
-          }
-          console.log(stats)
+
+          //Sorting data =)
+          log.transactions.sort(function (a, b) {
+            return new Date(a.createDate.substring(0, 10)) - new Date(b.createDate.substring(0, 10));
+          });
+
+          console.log(createDateSeries(log));
+
+
       });
 })
+
+function createDateSeries(log) {
+  //Creating a series of day data
+  var startDate = new Date("2016-12-31")
+  var dates = []
+  var sums = []
+  for (var i = 0; i < 30; i++) {
+    dates.unshift(new Date(startDate - i*86400000))
+    sums.push(0)
+  }
+  var j = log.transactions.length-1;
+  for (var i = dates.length-1; i >= 0; i--) {
+    while (simpleCompareDate(new Date(log.transactions[j].createDate), dates[i])){
+      sums[i] += parseInt(log.transactions[j].amount)
+      j--;
+    }
+  }
+  var chartData = {
+    labels: dates,
+    values: sums
+  }
+  return chartData
+}
+
+function simpleCompareDate(d1, d2) {
+  if (d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear()) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 app.get("/test", function(req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
